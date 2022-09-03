f03a910e6aedef00cbf1e9de316aa2cfeeb1bbb2
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-03 11:42:43 +0800

    Initial Commit

---
+++ SwiftUIGenerateQrCode/SwiftUIGenerateQrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGenerateQrCodeApp.swift
+//  SwiftUIGenerateQrCode
+//
+//  Created by Lucy on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGenerateQrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
